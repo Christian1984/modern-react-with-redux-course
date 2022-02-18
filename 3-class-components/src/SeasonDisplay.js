34bdb369c1e595984060de3f31e2bf1c7b0eb6dc
Chris <chris.hoffmann84@gmail.com>
2022-02-18 17:30:00 +0100

    added node-sass dependency, switched from css to scss

--- 3-class-components/src/SeasonDisplay.js
+++ 3-class-components/src/SeasonDisplay.js
@@ -1,24 +1,26 @@
-import "./SeasonDisplay.css";
+import "./SeasonDisplay.scss";
 import React from "react";
 
 const seasonConfig = {
     summer: {
         text: "Let's go to the beach",
-        iconName: "sun"
+        iconName: "sun",
+        className: "summer"
     },
     winter: {
         text: "Burrr it's cold!",
-        iconName: "snowflake"
+        iconName: "snowflake",
+        className: "winter"
     }
 }
 
 const SeasonDisplay = props => {
     const isSummer = (props.lat >= 0 && props.month > 2 && props.month < 9)
         || (props.lat < 0 && (props.month <= 2 || props.month >= 9));
-    const { text, iconName } = isSummer ? seasonConfig.summer : seasonConfig.winter;
+    const { text, iconName, className } = isSummer ? seasonConfig.summer : seasonConfig.winter;
 
     return (
-        <div className="season-display">
+        <div className={ `season-display ${className}` }>
             <i className= { `${iconName} icon massive icon-left` } ></i>
             <h1> { text }</h1>
             <i className= { `${iconName} icon massive icon-right` } ></i>
--- 3-class-components/src/index.js
+++ 3-class-components/src/index.js
@@ -21,12 +21,13 @@ class App extends React.Component {
 
     render() {
         console.log("render at " + new Date());
+        const month = 6;//new Date().getMonth()
 
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: { this.state.errorMessage }</div>
         }
         else if (this.state.lat) {
-            return <SeasonDisplay lat={ this.state.lat } month={ new Date().getMonth() } />;
+            return <SeasonDisplay lat={ this.state.lat } month={ month } />;
         }
         else if (!this.state.lat && !this.state.err) {
             return <div>Loading...</div>
