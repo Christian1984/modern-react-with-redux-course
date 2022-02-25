b6218277d4c62b848d068383435e5ffbc5900d72
Chris <chris.hoffmann84@gmail.com>
2022-02-26 00:23:49 +0100

    added form submit handler

--- 4-user-input/src/components/SearchBar.js
+++ 4-user-input/src/components/SearchBar.js
@@ -5,15 +5,20 @@ class SearchBar extends React.Component {
         searchTerm: ""
     };
 
+    onSubmitHandler(e) {
+        e.preventDefault();
+        console.log(this.state.searchTerm);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={ e => this.onSubmitHandler(e) }>
                     <div className="field">
                         <label htmlFor="search">Image Search</label>
                         <input 
                             type="text"
-                            name="search"
+                            name="searchTerm"
                             placeholder="Search Term"
                             value={ this.state.searchTerm }
                             onChange={ e => this.setState({searchTerm: e.target.value}) }
