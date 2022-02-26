ebe7d152dbe08d780015a1781875ad2c2cd3830a
Chris <chris.hoffmann84@gmail.com>
2022-02-26 08:56:09 +0100

    naive api call implementation

--- 4-user-input/src/components/App.js
+++ 4-user-input/src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import axios from "axios";
 import ImageList from "./ImageList";
 import SearchBar from "./SearchBar";
+import ApiKeys from "../nodist/ApiKeys";
 
 class App extends React.Component {
     onSearchHandler = searchTerm => {
-        console.log("searchTerm", searchTerm);
+        console.log("Searching images for: ", searchTerm);
+
+        axios.get("https://api.unsplash.com/search/photos/?query=" + searchTerm + "&client_id=" + ApiKeys.UNSPLASH_API_KEY)
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
     }
 
     render() {
