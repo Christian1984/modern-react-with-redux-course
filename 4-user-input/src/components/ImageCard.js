c8184f428a6732024df6205f401c56a29677ca6e
Chris <chris.hoffmann84@gmail.com>
2022-03-01 21:04:59 +0100

    improved grid styling

--- 4-user-input/src/components/ImageCard.js
+++ 4-user-input/src/components/ImageCard.js
@@ -13,13 +13,19 @@ class ImageCard extends React.Component {
         console.log(this.state.imgRef);
         this.state.imgRef.current.addEventListener("load", () => {
             console.log(this.state.imgRef.current.clientHeight);
-            const spans = Math.floor(this.state.imgRef.current.clientHeight / 10) + 1;
+            const spans = Math.floor(this.state.imgRef.current.clientHeight / 10);
             this.setState({ spans: spans });
         });
     }
 
     render() {
-        return <li style={ { gridRowEnd: `span ${this.state.spans}` } }><img ref={ this.state.imgRef } src={ this.props.url } alt={ this.props.alt } /></li>
+        return (
+            <li style={ { gridRowEnd: `span ${this.state.spans + 1}` } }>
+                <span style={ { height: `${(this.state.spans) * 10}px`} }>
+                    <img ref={ this.state.imgRef } src={ this.props.url } alt={ this.props.alt } />
+                </span>
+            </li>
+        );
     }
 }
 
