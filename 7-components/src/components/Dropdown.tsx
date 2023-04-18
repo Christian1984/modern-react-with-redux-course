2322bc03ac143198b84d12699ef185c22090a7a9
Christian Hoffmann <christian.hoffmann@sigmatechnology.com>
2023-04-18 09:44:14 +0200

    worked on dropdown

--- 7-components/src/components/Dropdown.tsx
+++ 7-components/src/components/Dropdown.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 type DropdownItemProps = {
   label: string;
@@ -6,11 +7,58 @@ type DropdownItemProps = {
 };
 
 type DropdownProps = {
+  label: string;
+  value: DropdownItemProps | null;
+  handleSelect: (selected: DropdownItemProps | null) => void;
   items: DropdownItemProps[];
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-const Dropdown = ({ items }: DropdownProps) => {
-  return <div>Dropdown</div>;
+const Dropdown = ({ label, items, value, handleSelect, className, ...rest }: DropdownProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const hide = () => setIsExpanded(false);
+
+  useEffect(() => {
+    document.addEventListener("click", hide);
+    return () => document.removeEventListener("click", hide);
+  }, []);
+
+  return (
+    <div className={className + " relative"} {...rest}>
+      <div>{label}</div>
+      <div
+        className="cursor-pointer flex items-center justify-between border p-3"
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsExpanded((prev) => !prev);
+        }}
+      >
+        <span>
+          {value && value.label}
+          {!value && "Color..."}
+        </span>
+        <span className="text-xl">
+          <GoChevronDown />
+        </span>
+      </div>
+      {isExpanded && (
+        <div className="border absolute bg-white w-full">
+          {items.map((item) => (
+            <div
+              key={item.value}
+              className="cursor-pointer p-3 hover:bg-gray-50"
+              onClick={() => {
+                handleSelect(item);
+                // setIsExpanded(false);
+              }}
+            >
+              {item.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export { Dropdown };
--- 7-components/src/pages/DropdownPage.tsx
+++ 7-components/src/pages/DropdownPage.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import { Dropdown, DropdownItemProps } from "../components/Dropdown";
 
 const DropdownPage = () => {
-  const items: DropdownItemProps[] = [{ label: "Can I use React on this Project", value: "Yes!" }];
-  return <Dropdown items={items} />;
+  const [selected, setSelected] = useState<DropdownItemProps | null>(null);
+
+  const items: DropdownItemProps[] = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  return (
+    <>
+      <div className="m-3" onClick={() => console.log("click")}>
+        <Dropdown label="Pick a Color" items={items} value={selected} handleSelect={setSelected} />
+      </div>
+      <div className="m-3">
+        Selected: <span className={`text-${selected ? selected.value : "black"}-500`}>{selected?.label}</span>
+      </div>
+    </>
+  );
 };
 
 export { DropdownPage };
